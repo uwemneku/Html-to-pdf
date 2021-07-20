2f61a75f12de82244292e9c480aac00c5b72b2fb
uwem <uwemneku@gmail.com>
2021-07-20 01:26:58 +0100

    set up

---
+++ src/Example.js
@@ -0,0 +1,56 @@
+import React from "react";
+import ReactToPrint from "react-to-print";
+
+const thStyle = {
+  fontFamily: "Anton",
+  fontWeight: "normal",
+  fontStyle: "normal",
+  // margin: "20px",
+};
+
+class ComponentToPrint extends React.Component {
+  render() {
+    return (
+      <table style={{margin:20}}>
+        <thead style={thStyle}>
+          <th>column 1</th>
+          <th>column 2</th>
+          <th>column 3</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>data 1</td>
+            <td>data 2</td>
+            <td>data 3</td>
+          </tr>
+          <tr>
+            <td>data 1</td>
+            <td>data 2</td>
+            <td>data 3</td>
+          </tr>
+          <tr>
+            <td>data 1</td>
+            <td>data 2</td>
+            <td>data 3</td>
+          </tr>
+        </tbody>
+      </table>
+    );
+  }
+}
+
+class Example extends React.Component {
+  render() {
+    return (
+      <div>
+        <ReactToPrint
+          trigger={() => <button>Print this out!</button>}
+          content={() => this.componentRef}
+        />
+        <ComponentToPrint ref={(el) => (this.componentRef = el)} />
+      </div>
+    );
+  }
+}
+
+export default Example;
